72689771a5f599a5f1cfa905d83a861821cb8c13
shiwasu-tech <njayamena@gmail.com>
2024-11-29 09:11:43 +0900

    first commit add main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike.control import wait_for_seconds, wait_until, Timer
+from math import *
+
+hub = PrimeHub()
